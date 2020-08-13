82757bd4550e3a5f72d005187f3114e49f743eef
hyin08 <jeff.yin2016@gmail.com>
2020-08-12 20:30:57 -0700

    6. 用token构建DOM树 7. 将文本节点加到DOM树

--- week03/HTTP/parser.js
+++ week03/HTTP/parser.js
@@ -1,9 +1,53 @@
 let currentToken = null;
 let currentAttribute = null;
+let currentTextNode = null;
+
+let stack = [{ type: "document", children:[] }];
 
 function emit(token) {
-    if(token.type != "text") {
-        console.log(token);
+    let top = stack[stack.length - 1];
+
+    if(token.type == "startTag") {
+        let element = {
+            type: "element",
+            children: [],
+            attributes: [],
+        };
+
+        element.tagName = token.tagName;
+
+        for(let p in token) {
+            if(p != "type" && p != "tagName") {
+                element.attributes.push({
+                    name: p,
+                    value: token[p]
+                });
+            }
+        }
+
+        top.children.push(element);
+        element.parent = top;
+
+        if(!token.isSelfClosing)
+            stack.push(element);
+
+        currentTextNode = null;
+    } else if(token.type == "endTag") {
+        if(top.tagName != token.tagName) {
+            throw new Error("Tag start end doesn't match");
+        } else {
+            stack.pop();
+        }
+        currentTextNode = null;
+    } else if(token.type == "text") {
+        if(currentTextNode == null) {
+            currentTextNode = {
+                type: "text",
+                content: ""
+            }
+            top.children.push(currentTextNode);
+        }
+        currentTextNode.content += token.content;
     }
 }
 
@@ -226,4 +270,5 @@ module.exports.parseHTML = function parseHTML(html) {
         state = state(c);
     }
     state = state(EOF);
+    console.log(stack[0]);
 }
