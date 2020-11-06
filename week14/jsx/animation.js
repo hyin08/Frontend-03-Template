c70f222781f29b8ae1ef6b9aa93364cb4ced3679
hyin08 <jeff.yin2016@gmail.com>
2020-11-06 00:44:15 -0800

    设计时间线的更新

--- week14/jsx/animation.js
+++ week14/jsx/animation.js
@@ -1,26 +1,31 @@
 const TICK = Symbol("tick");
 const TICK_HANDLER = Symbol("tick-handler");
 const ANIMATIONS = Symbol("animations");
-
+const START_TIME = Symbol("start-time");
 export class Timeline {
     constructor() {
         this[ANIMATIONS] = new Set();
+        this[START_TIME] = new Map();
     }
 
     start() {
         let startTime = Date.now();
          // 使用Symbol, 避免外面不小心访问
         this[TICK] = () => {
-            let t = Date.now() - startTime;
+            let now = Date.now()
             for(let animation of this[ANIMATIONS]) {
-                let t0 = t;
+                let t;
+                if(this[START_TIME].get(animation) < startTime)
+                    t = now - startTime;
+                else // 运行过程中可以添加动画
+                    t = now - this[START_TIME].get(animation);
                 // 终止当前animation
                 if(animation.duration < t) {
                     this[ANIMATIONS].delete(animation);
                     // 处理最后超出duration范围的问题
-                    t0 = animation.duration;
+                    t = animation.duration;
                 }
-                animation.receive(t0);
+                animation.receive(t);
             }
             requestAnimationFrame(this[TICK]);
         }
@@ -40,18 +45,22 @@ export class Timeline {
 
     reset() {}
 
-    add(animation) {
+    add(animation, startTime) {
+        if(arguments.length < 2)
+            startTime = Date.now();
         this[ANIMATIONS].add(animation);
+        this[START_TIME].set(animation, startTime);
     }
 }
 
 export class Animation {
-    constructor(object, property, startValue, endValue, duration, timingFunction) {
+    constructor(object, property, startValue, endValue, duration, delay, timingFunction) {
         this.object = object;
         this.property = property;
         this.startValue = startValue;
         this.endValue = endValue;
         this.duration = duration;
+        this.delay = delay;
         this.timingFunction = timingFunction;
     }
 
