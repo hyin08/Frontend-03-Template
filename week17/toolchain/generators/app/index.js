bfbbd5ab49c1996f60ab6b5420216761d292f615
hyin08 <jeff.yin2016@gmail.com>
2020-11-28 22:05:47 -0800

    yeoman - managing dependencies

--- week17/toolchain/generators/app/index.js
+++ week17/toolchain/generators/app/index.js
@@ -35,6 +35,24 @@ module.exports = class extends Generator {
     this.log("cool feature", answers.cool);
   }
 
+  initPackage() {
+    const pkgJson = {
+      devDependencies: {
+        eslint: '^3.15.0'
+      },
+      dependencies: {
+        react: '^16.2.0'
+      }
+    };
+
+    // Extend or create package.json file in destination path
+    this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
+  }
+
+  install() {
+    this.npmInstall();
+  }
+
   writing() {
     this.fs.copyTpl(
       this.templatePath('index.html'),
