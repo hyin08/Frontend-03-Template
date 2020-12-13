587791ffc539504290b8b1769c1d2bc53c4ce376
hyin08 <jeff.yin2016@gmail.com>
2020-12-13 04:03:03 -0800

    用GitHub oAuth做一个登录

--- week19/publish-server/server.js
+++ week19/publish-server/server.js
@@ -1,14 +1,84 @@
 let http = require('http');
+let https = require('https');
 // let fs = require('fs');
 let unzipper = require('unzipper');
+let querystring = require('querystring');
 
-http.createServer(function(req, res) {
-    console.log(req.headers);
+// 2. auth路由：接收code， 用code + client_id + client_secret 换 token
+function auth(req, res) {
+    let query = querystring.parse(req.url.match(/^\/auth\?([\s\S]+)$/)[1]);
+    getToken(query.code, function (info) {
+        console.log(info);
+        res.write(`<a href='http://localhost:8083/?token=${info.access_token}'>publish</a>`);
+        res.end();
+    });
 
+}
+
+function getToken(code, callback) {
+    let request = https.request({
+        hostname: 'github.com',
+        path: `/login/oauth/access_token?code=${code}&client_id=Iv1.d1d27d9bc42c9e1f&client_secret=d13eb53d1062d27faa6b5059d380fac89d532cfb`,
+        port: 443,
+        method: 'POST',
+    }, function (response) {
+        let body = '';
+        response.on('data', chunk => {
+            body += chunk.toString()
+        })
+
+        response.on('end', chunk => {
+            callback(querystring.parse(body));
+        })
+    });
+    request.end();
+}
+// 4. publish路由： 用token获取用户信息，检查权限，接收发布
+function publish(req, res) {
+    let query = querystring.parse(req.url.match(/^\/publish\?([\s\S]+)$/)[1]);
+    
+    getUser(query.token, info => {
+        if(info.login === 'hyin08') {
+            req.pipe(unzipper.Extract({ path: '../server/public/' }));
+        }
+    });    
+}
+
+function getUser(token, callback) {
+    let request = https.request({
+        hostname: 'api.github.com',
+        path: `/user`,
+        port: 443,
+        method: 'GET',
+        headers: { 
+            Authorization: `token ${token}`,
+            "User-Agent": 'toy-publish'
+        }
+    }, function (response) {
+        let body = '';
+        response.on('data', chunk => {
+            body += chunk.toString()
+        })
+
+        response.on('end', chunk => {
+            callback(JSON.parse(body));
+        })
+    });
+    request.end();
+}
+
+
+
+http.createServer(function (req, res) {
+
+    if (req.url.match(/^\/auth\?/))
+        return auth(req, res);
+    if (req.url.match(/^\/publish\?/))
+        return publish(req, res);
     // let outFile = fs.createWriteStream("../server/public/file.zip")
 
     // req.pipe(outFile);
 
-    req.pipe(unzipper.Extract({ path: '../server/public/' }));
+    // req.pipe(unzipper.Extract({ path: '../server/public/' }));
 
 }).listen(8082)
--- week19/publish-tool/publish.js
+++ week19/publish-tool/publish.js
@@ -1,31 +1,40 @@
 let http = require('http');
 let fs = require('fs');
 let archiver = require('archiver');
+let child_process = require('child_process');
+let querystring = require('querystring');
 
-let request = http.request({
-    hostname: "127.0.0.1",
-    port: 8082,
-    method: "POST",
-    headers: {
-        'Content-Type': 'application/octet-stream'
-    }
-}, response => {
-    console.log(response);
-});
+// 1. 打开 https://github.com/login/oauth/authorize
 
-// let file = fs.createReadStream("./sample.html");
+child_process.exec(`open https://github.com/login/oauth/authorize?client_id=Iv1.d1d27d9bc42c9e1f`)
 
+// 3. 创建server， 接收token， 后点击发布
 
-const archive = archiver('zip', {
-    zlib: { level: 9 } // Sets the compression level.
-  });
+http.createServer(function(req, res) {
+    let query = querystring.parse(req.url.match(/^\/\?([\s\S]+)$/)[1]);
+    publish(query.token);
 
-archive.directory('sample/', false);
-archive.finalize();
-archive.pipe(request);
+}).listen(8083)
 
-// file.pipe(request);
-
-// file.on('end', () => {
-//     request.end()
-// })
+function publish(token) {
+    let request = http.request({
+        hostname: "127.0.0.1",
+        port: 8082,
+        method: "POST",
+        path: '/publish?token=' + token,
+        headers: {
+            'Content-Type': 'application/octet-stream'
+        }
+    }, response => {
+        console.log(response);
+    });
+    
+    
+    const archive = archiver('zip', {
+        zlib: { level: 9 } // Sets the compression level.
+      });
+    
+    archive.directory('sample/', false);
+    archive.finalize();
+    archive.pipe(request);
+}
